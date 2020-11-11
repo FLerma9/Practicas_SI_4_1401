361421df25e6ad704db42c1c6f8dc73e632ffdee
FLerma9 <francisco.lermam@estudiante.uam.es>
2020-11-11 17:28:13 +0100

    ejercicio f

--- P2/actualiza.sql
+++ P2/actualiza.sql
@@ -5,9 +5,9 @@ ALTER income SET NOT NULL,
 ALTER gender SET NOT NULL;
 
 ALTER TABLE imdb_actormovies 
---ADD CONSTRAINT imdb_actormovies_pkey PRIMARY KEY (actorid, movieid),
---ADD CONSTRAINT  imdb_actormovies_actorid_fkey FOREIGN KEY (actorid) REFERENCES imdb_actors(actorid) ON UPDATE CASCADE ON DELETE CASCADE,
---ADD CONSTRAINT  imdb_actormovies_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE, 
+ADD CONSTRAINT imdb_actormovies_pkey PRIMARY KEY (actorid, movieid),
+ADD CONSTRAINT  imdb_actormovies_actorid_fkey FOREIGN KEY (actorid) REFERENCES imdb_actors(actorid) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT  imdb_actormovies_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE, 
 ALTER isvoice DROP DEFAULT,
 ALTER isvoice TYPE bool USING (isvoice::int::bool),
 ALTER isvoice SET NOT NULL,
@@ -57,22 +57,86 @@ ALTER ishead SET NOT NULL,
 ALTER ishead SET DEFAULT false;
 
 
+CREATE SEQUENCE countries_countryid_seq;
+CREATE TABLE countries(
+  countryid integer NOT NULL DEFAULT nextval('countries_countryid_seq'),
+  country character varying(32) NOT NULL,
+  CONSTRAINT countries_pkey PRIMARY KEY (countryid)
+);
+
+INSERT INTO countries (country)
+SELECT DISTINCT country FROM imdb_moviecountries;
+
+CREATE SEQUENCE imdb_moviecountries_countryid_seq;
 
 ALTER TABLE imdb_moviecountries
+ADD COLUMN countryid integer,
 DROP CONSTRAINT  imdb_moviecountries_movieid_fkey,
-ADD CONSTRAINT imdb_moviecountries_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE;
+ADD CONSTRAINT imdb_moviecountries_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT imdb_moviecountries_countryid_fkey FOREIGN KEY (countryid) REFERENCES countries(countryid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+UPDATE imdb_moviecountries
+SET countryid = t.countryid
+FROM countries as t
+WHERE t.country = imdb_moviecountries.country;
+
+ALTER TABLE imdb_moviecountries
+DROP COLUMN country;
+
+
+
+CREATE SEQUENCE genres_genreid_seq;
+CREATE TABLE genres(
+  genresid integer NOT NULL DEFAULT nextval('genres_genreid_seq'),
+  genre character varying(32) NOT NULL,
+  CONSTRAINT genres_pkey PRIMARY KEY (genresid)
+);
+
+INSERT INTO genres (genre)
+SELECT DISTINCT genre FROM imdb_moviegenres;
 
 
 ALTER TABLE imdb_moviegenres
+ADD COLUMN genresid integer,
 DROP CONSTRAINT  imdb_moviegenres_movieid_fkey,
-ADD CONSTRAINT imdb_moviegenres_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE;
+ADD CONSTRAINT imdb_moviegenres_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT imdb_moviegenres_genresid_fkey FOREIGN KEY (genresid) REFERENCES genres(genresid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+UPDATE imdb_moviegenres
+SET genresid = t.genresid
+FROM genres as t
+WHERE t.genre = imdb_moviegenres.genre;
+
+ALTER TABLE imdb_moviegenres
+DROP COLUMN genre;
+
+
+CREATE SEQUENCE languages_languageid_seq;
+CREATE TABLE languages(
+  languageid integer NOT NULL DEFAULT nextval('languages_languageid_seq'),
+  language character varying(32) NOT NULL,
+  CONSTRAINT languages_pkey PRIMARY KEY (languageid)
+);
+
+INSERT INTO languages (language)
+SELECT DISTINCT language FROM imdb_movielanguages;
 
 
 ALTER TABLE imdb_movielanguages
+ADD COLUMN languageid integer,
 DROP CONSTRAINT  imdb_movielanguages_pkey,
 ADD CONSTRAINT imdb_movielanguages_pkey PRIMARY KEY (movieid, language),
 DROP CONSTRAINT  imdb_movielanguages_movieid_fkey,
-ADD CONSTRAINT imdb_movielanguages_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE;
+ADD CONSTRAINT imdb_movielanguages_movieid_fkey FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT imdb_movielanguages_languageid_fkey FOREIGN KEY (languageid) REFERENCES languages(languageid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+UPDATE imdb_movielanguages
+SET languageid = t.languageid
+FROM languages as t
+WHERE t.language = imdb_movielanguages.language;
+
+ALTER TABLE imdb_movielanguages
+DROP COLUMN language;
 
 
 
@@ -83,20 +147,26 @@ ALTER issuspended TYPE bool USING (issuspended::int::bool),
 ALTER issuspended SET NOT NULL,
 ALTER issuspended SET DEFAULT false;
 
---ALTER TABLE inventory
---ADD CONSTRAINT inventory_prod_id_fkey FOREIGN KEY (prod_id) REFERENCES products(prod_id) ON UPDATE CASCADE ON DELETE CASCADE;
---ADD CONSTRAINT valid_stock CHECK (stock >= 0),
---ADD CONSTRAINT valid_sales CHECK (sales >= 0);
+ALTER TABLE inventory
+ADD CONSTRAINT inventory_prod_id_fkey FOREIGN KEY (prod_id) REFERENCES products(prod_id) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT valid_stock CHECK (stock >= 0),
+ADD CONSTRAINT valid_sales CHECK (sales >= 0);
 
+--SELECT orderid, prod_id, price, SUM(quantity)
+--INTO temporal
+--FROM orderdetail
+--GROUP BY orderid, prod_id
+--ORDER BY COUNT(orderid, prod_id);
 
---ALTER TABLE orderdetail
---ADD CONSTRAINT orderdetail_orderid_fkey FOREIGN KEY (orderid) REFERENCES orders(orderid) ON UPDATE CASCADE ON DELETE CASCADE,
---ADD CONSTRAINT orderdetail_prod_id_fkey FOREIGN KEY (prod_id) REFERENCES products(prod_id) ON UPDATE CASCADE ON DELETE CASCADE,
---ADD CONSTRAINT valid_quantity CHECK (quantity >= 0);
+ALTER TABLE orderdetail
+--ADD CONSTRAINT orderdetail_pkey PRIMARY KEY (orderid, prod_id);
+ADD CONSTRAINT orderdetail_orderid_fkey FOREIGN KEY (orderid) REFERENCES orders(orderid) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT orderdetail_prod_id_fkey FOREIGN KEY (prod_id) REFERENCES products(prod_id) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT valid_quantity CHECK (quantity >= 0);
 
---ALTER TABLE orders
---ADD CONSTRAINT orders_customerid_fkey FOREIGN KEY (customerid) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE,
---ADD CONSTRAINT valid_tax CHECK (tax >= 0);
+ALTER TABLE orders
+ADD CONSTRAINT orders_customerid_fkey FOREIGN KEY (customerid) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE,
+ADD CONSTRAINT valid_tax CHECK (tax >= 0);
 
 
 ALTER TABLE products

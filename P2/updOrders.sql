abd1709edbd99eff5c6d679610b9531af5184194
Jesus Morato <jesusmoratomartin@gmail.com>
2020-11-25 18:42:42 +0100

    Trigger

--- P2/updOrders.sql
+++ P2/updOrders.sql
@@ -1,13 +1,21 @@
 ﻿DROP TRIGGER IF EXISTS updOrders ON orderdetail;
-DROP FUNCTION IF EXISTS updOrders();
+DROP FUNCTION IF EXISTS updOrders() CASCADE;
 CREATE OR REPLACE FUNCTION updOrders() RETURNS TRIGGER AS $updOrders$
     BEGIN
 
     IF (TG_OP = 'DELETE') THEN
-      IF ((SELECT orderid  FROM orderdetail WHERE orderid = OLD.orderid) IS NULL)
+      IF ((SELECT DISTINCT orderid  FROM orderdetail WHERE orderid = OLD.orderid) IS NULL)
           THEN DELETE FROM orders WHERE orderid = OLD.orderid;
       RETURN NEW;
       END IF;
+
+      UPDATE orders
+      SET netamount = nuevo.sum,
+          totalamount = nuevo.sum + nuevo.sum*(tax/100)
+      FROM ((SELECT SUM (price*quantity) AS sum FROM orderdetail as o
+              WHERE o.orderid = OLD.orderid)) AS nuevo
+      WHERE orders.orderid = OLD.orderid;
+      RETURN NEW;
     END IF;
 
     UPDATE orders
